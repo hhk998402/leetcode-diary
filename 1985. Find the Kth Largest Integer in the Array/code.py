56cea7e1520ea3a36fd9493ff57c9e40e03f54d5
hhk998402 <hemant.hari.kumar@gmail.com>
2023-02-01 23:33:53 -0500

    Solved 1985. Find the Kth Largest Integer in the Array

---
+++ 1985. Find the Kth Largest Integer in the Array/code.py
@@ -0,0 +1,7 @@
+import heapq
+
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums = [int(x) for x in nums]
+        heapq.heapify(nums)
+        return(str(heapq.nlargest(k, nums)[-1]))
